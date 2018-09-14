14be25d5bc35327c2d8a18354bcbb1b18885dc70
ASVBPREAUBV <paul@paul-mayer.eu>
2018-09-14 20:20:17 +0200

    set GET Request

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # golang test repo
 
+
+
 ## Todo
 - [x] Hello World
 - [ ] install packages
 - [ ] manage packages
 - [ ] recieve GET request
-- [ ] send GET request
+- [x] send GET request
--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,16 @@
 package main
 import "fmt"
+import "net/http"
+
 func main() {
     fmt.Println("hello world")
+    resp, err := http.Get("http://example.com/")
+    if err != nil {
+    	// handle error
+    	fmt.Println(err)
+
+    }
+    fmt.Println(resp)
 }
+
+
