c5acb64f083660b3e75d736f155958f320de16e2
ASVBPREAUBV <paul@paul-mayer.eu>
2018-09-17 12:21:31 +0200

    Init MUX

--- hello-world.go
+++ hello-world.go
@@ -1,16 +1,31 @@
 package main
-import "fmt"
-import "net/http"
 
-func main() {
-    fmt.Println("hello world")
-    resp, err := http.Get("http://example.com/")
-    if err != nil {
-    	// handle error
-    	fmt.Println(err)
+import (
+    "fmt"
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
+)
 
-    }
-    fmt.Println(resp)
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello World!\n"))
 }
 
+func main() {
 
+        fmt.Println("hello world")
+        resp, err := http.Get("http://example.com/")
+        if err != nil {
+        	// handle error
+        	fmt.Println(err)
+
+        }
+        fmt.Println(resp)
+
+    r := mux.NewRouter()
+    // Routes consist of a path and a handler function.
+    r.HandleFunc("/", YourHandler)
+
+    // Bind to a port and pass our router in
+    log.Fatal(http.ListenAndServe(":8000", r))
+}
