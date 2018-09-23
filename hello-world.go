f7817374aa517b00bf25a7a40900dabedf74267d
ASVBPREAUBV <paul@paul-mayer.eu>
2018-09-23 09:21:20 +0200

    init sqlite

--- hello-world.go
+++ hello-world.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
+	"strconv"
 )
 
 type request_query struct {
 	Id         int      `json:"id"`
 	Stringlist []string `json:"stringlist"`
+
 }
 
 func Pong(c *gin.Context) {
@@ -49,11 +53,27 @@ func YourHandler(c *gin.Context) {
 
 func main() {
 
+	database, _ := sql.Open("sqlite3", "./test.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Nic", "Raboy")
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+
+
 	router := gin.Default()
 	router.GET("/ping", Pong)
 
 	router.GET("/", YourHandler)
 	router.Run()
+
 
 	// Bind to a port and pass our router in
 	//log.Fatal(http.ListenAndServe(":8000", router))
